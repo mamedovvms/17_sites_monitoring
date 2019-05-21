63f448506a0829e28b97ee94cc81ed5d33ba035c
1237 <mamedov_vs@oksw.ru>
2019-05-21 18:27:45 +0700

    Добавлены апострофы вокруг url при выводе ошибки

--- check_sites_health.py
+++ check_sites_health.py
@@ -40,6 +40,7 @@ def get_domain_expiration_date(domain_name):
 
 
 def is_expiration_date_valid(expiration_date, delta):
+    #не могу понять почему здесь должен быть список
     if not expiration_date or not isinstance(expiration_date, datetime):
         return False
     today = datetime.utcnow()
